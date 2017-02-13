b804fa727bce331bbc4ea54e0d447ff0ab103e44
Jeb Bearer <jbearer@hmc.edu>
2017-02-12 20:14:03 -0800

    Secure comment feature.
    The only people who can delete a comment are the author of the comment
    and the author of the article.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,13 +8,18 @@ class CommentsController < ApplicationController
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
 
-    redirect_to article_path(@article)
+    # A comment can be deleted by the author of the comment or of the article
+    if current_user == @comment.user or current_user == @article.user
+      @comment.destroy
+      redirect_to article_path(@article)
+    else
+      redirect_to article_path(@article)
+    end
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body).merge(user_id: current_user.id)
     end
 end
